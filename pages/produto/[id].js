a9c9437015a46c764a078501664378fbd652b4b8
Vanessa Bueno <vanessa.webdeveloper@gmail.com>
2025-01-17 12:18:00 -0300

    Adicionando routa dinâmica de produtos

--- pages/produto/[id].js
+++ pages/produto/[id].js
@@ -1,5 +1,28 @@
-export async function getServerSideProps(context) {
-    const id = context.query.id;
+export async function getStaticPaths(params) {
+    return {
+        paths: [
+            {
+                params: {
+                    id: '1'
+                }            
+            },
+            {
+                params : {
+                    id: '2'
+                }
+            }
+        ],
+        fallback: 'blocking' 
+        // false = desabilita o fallback, qualquer path não listado retornará 404, 
+        // true = o nextjs aceitará qualquer path que acessar a rota dinâmica e vai gerar a página de forma assíncrona
+        // blocking = assim como o "getServerSideProps" vai bloquear a requisição da página até que consiga com sucesso gerar a página final (response) e assim retornar.
+        // Uma vez a página gerada, seja por fallback assíncrono ou por blocking, esta página ficará em cache, retornando assim as próximas requisições instantaneamente!
+    }
+}
+
+export async function getStaticProps(context) {
+    await delay(5000); // demora artificial
+    const id = context.params.id;
 
     return {
         props: {
@@ -18,3 +41,9 @@ function Produtos(props) {
 }
 
 export default Produtos;
+
+
+// Gerando um delay propositalmente, para teste.
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
